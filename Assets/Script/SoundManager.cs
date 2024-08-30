8e26df5195671be901d8fd531fc1435de21fa988
penguinjean0421 <penguinjean0421@gmail.com>
2024-08-30 22:02:31 +0900

    Update SoundManager.cs

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class SoundManager
 {
+    // 정답 타이핑
+    public void CorrectAnswerTyping()
+    { 
+
+    }
+
+    // 오답 타이핑
+    public void WrongAnswerTyping()
+    {
+
+    }
+
+    // 전체적으로 깔리는 배경음악
+    public void BackGroundMusic()
+    { 
+
+    }
+
+    //  환경마다 달라지는 배경음악
+    public void EnviromentMusic()
+    { 
     
+    }
 }
