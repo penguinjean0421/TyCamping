3ad33437807199eb0284f35400141fd0593d3023
DESKTOP-6HHQMD1\KHJ <khjj07@gmail.com>
2024-09-01 00:30:17 +0900

    sdf 에러

--- Assets/Script/Game/Stage1.cs
+++ Assets/Script/Game/Stage1.cs
@@ -30,7 +30,11 @@ namespace Assets.Script.Game
                 snode.spriteGroup.SetActive(false);
                 snode.hint.gameObject.SetActive(false);
             }
+
             //사운드 BGM
+            AudioManager.instance.PlayBGM(true);
+            AudioManager.instance.PlayEnvirBgm(AudioManager.EnvirBgm.Stage11, true);
+
         }
 
         public void OnCutActive0()
@@ -182,6 +186,8 @@ namespace Assets.Script.Game
             snodeList[9].spriteGroup.transform.GetChild(0).position = snodeList[9].spriteGroup.transform.GetChild(0).position - Vector3.up * 0.5f;
             snodeList[9].spriteGroup.transform.GetChild(0).DOMoveY(0.5f, 0.5f).SetRelative();
             Debug.Log("우리가족의 첫 캠핑");
+
+            AudioManager.instance.PlayEnvirBgm(AudioManager.EnvirBgm.Stage11, false);
         }
     }
 }
