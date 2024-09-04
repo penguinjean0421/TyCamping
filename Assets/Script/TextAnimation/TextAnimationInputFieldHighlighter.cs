f25365c52274c5b22babe9ed22f8ec350debe6d3
okgun77 <okgun2naver.com>
2024-09-04 16:23:40 +0900

    - 이것저것 테스트 중
      : 타이핑 텍스트애니메이션 오류 확인중...

--- Assets/Script/TextAnimation/TextAnimationInputFieldHighlighter.cs
+++ Assets/Script/TextAnimation/TextAnimationInputFieldHighlighter.cs
@@ -24,6 +24,7 @@ public class TextAnimationInputFieldHighlighter : MonoBehaviour
     private Tween rotationHandler;
     public void Start()
     {
+        DOTween.Init();
         _inputField = GetComponent<TMP_InputField>();
         _printer = GetComponentInChildren<TextAnimationPrinter>();
         //_inputField.onValueChanged.AddListener(Highlight);
@@ -65,8 +66,13 @@ public class TextAnimationInputFieldHighlighter : MonoBehaviour
             {
                 rotationHandler.Rewind();
             }
-            scaleHandler = _printer.RepeatScaleTween(text.Length - 2, Vector3.one, Vector3.one * 2f, 0.1f, Ease.InBounce, LoopType.Yoyo, 2);
-            rotationHandler = _printer.RepeatRotationTween(text.Length - 2, Vector3.zero, Vector3.forward * 25, 0.1f, Ease.InOutBounce, LoopType.Yoyo, 2);
+
+            int index = Mathf.Max(0, text.Length - 2); // 음수 인덱스를 방지
+            scaleHandler = _printer.RepeatScaleTween(index, Vector3.one, Vector3.one * 2f, 0.1f, Ease.InBounce, LoopType.Yoyo, 2);
+            rotationHandler = _printer.RepeatRotationTween(index, Vector3.zero, Vector3.forward * 25, 0.1f, Ease.InOutBounce, LoopType.Yoyo, 2);
+
+            // scaleHandler = _printer.RepeatScaleTween(text.Length - 2, Vector3.one, Vector3.one * 2f, 0.1f, Ease.InBounce, LoopType.Yoyo, 2);
+            // rotationHandler = _printer.RepeatRotationTween(text.Length - 2, Vector3.zero, Vector3.forward * 25, 0.1f, Ease.InOutBounce, LoopType.Yoyo, 2);
         }
     }
     public void RematchText()
