ada8c5b2899feb0e30a5ca5c91f2266326399a4a
penguinjean0421 <penguinjean0421@gmail.com>
2024-09-11 11:16:37 +0900

    new Sound Manager

---
+++ Assets/Script/Sound/Channel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Default.Scripts.Sound
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Channel : MonoBehaviour
+    {
+        [Range(0.0f, 1.0f)]
+        public float volume = 0.5f;
+        private AudioSource _audioSource;
+
+        public void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Play(AudioClip clip)
+        {
+            _audioSource.clip = clip;
+            _audioSource.volume = volume;
+            _audioSource.Play();
+        }
+        public void PlayOneShot(AudioClip clip)
+        {
+            _audioSource.volume = volume;
+            _audioSource.PlayOneShot(clip);
+        }
+
+        public void Stop()
+        {
+            _audioSource.Stop();
+        }
+
+        public void Pause()
+        {
+            _audioSource.Pause();
+        }
+
+        public void SetVolume(float v)
+        {
+            volume = v;
+            _audioSource.volume = volume;
+        }
+    }
+}
---
+++ Assets/Script/Sound/SoundListAsset.cs
@@ -0,0 +1,49 @@
+﻿using Default.Scripts.Extension;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Default.Scripts.Sound
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(SoundListAsset))]
+    public class SoundListAssetInspector : Editor
+    {
+       
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var sl = (SoundListAsset)target;
+            if (GUILayout.Button("Load All Sound"))
+            {
+                var clips = sl.floder.LoadAllObjectsInFolder<AudioClip>();
+                sl.sounds = new List<Sound>();
+                foreach (var clip in clips)
+                {
+                    sl.sounds.Add(new Sound(clip));
+                }
+            }
+        }
+    }
+#endif
+    [CreateAssetMenu(fileName = "Sound List Asset",menuName = "Sound/Sound LIst Asset")]
+    public class SoundListAsset : ScriptableObject
+    {
+        public List<Sound> sounds;
+        public DefaultAsset floder;
+        public Sound GetSoundByName(string name)
+        {
+            var soundsQuery = from sound in sounds
+                where sound.name == name
+                select sound;
+            foreach (var sound in soundsQuery)
+            {
+                return sound;
+            }
+
+            return null;
+        }
+    }
+}
