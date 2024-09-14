a7b46233d267733b5dfaa7e9204498238e20ac7b
DESKTOP-0PER6KS\khjj0 <khjj07@gmail.com>
2024-09-14 19:00:19 +0900

    타이틀 작업 반영

--- Assets/Script/UI/TitleManager.cs
+++ Assets/Script/UI/TitleManager.cs
@@ -17,8 +17,9 @@ public class TitleManager : MonoBehaviour
     [Header("[Images]")]
     public Image map;
     public Image mapContentsMask;
-    public Image title;
+    public Image[] title;
     public Image character;
+    public Image tent;
     public RectTransform characterInitialPoint;
 
     private bool isOptionMenuEnabled = false;
@@ -37,7 +38,15 @@ public class TitleManager : MonoBehaviour
         var characterTargetPosition = character.rectTransform.localPosition;
         map.transform.localScale = Vector3.up;
         mapContentsMask.rectTransform.sizeDelta = Vector2.zero;
-        title.transform.localScale = Vector3.zero;
+
+        title[0].transform.localScale=Vector3.zero;
+        title[1].transform.localScale=Vector3.zero;
+        title[2].transform.localScale=Vector3.zero;
+        title[3].transform.localScale=Vector3.zero;
+        title[4].transform.localScale=Vector3.up;
+        tent.transform.localScale=Vector3.zero;
+
+
 
         startButton.transform.localScale = Vector3.zero;
         optionButton.transform.localScale = Vector3.zero;
@@ -48,7 +57,23 @@ public class TitleManager : MonoBehaviour
         animateSequence.Append(mapContentsMask.rectTransform.DOSizeDelta(map.rectTransform.sizeDelta*2, 2.0f).SetEase(Ease.InOutCubic));
         
         animateSequence.Append(character.transform.DOLocalJump(characterTargetPosition, 50,5,2));
-        animateSequence.Append(title.transform.DOScale(Vector3.one, 1f).SetEase(Ease.OutElastic));
+        animateSequence.Append(title[0].transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutElastic));
+        animateSequence.Join(title[1].transform.DOScale(Vector3.one, 1).SetDelay(0.1f).SetEase(Ease.OutElastic));
+        animateSequence.Join(title[2].transform.DOScale(Vector3.one, 1).SetDelay(0.2f).SetEase(Ease.OutElastic));
+        animateSequence.Join(title[3].transform.DOScale(Vector3.one, 1).SetDelay(0.3f).SetEase(Ease.OutElastic));
+        animateSequence.Append(tent.transform.DOScaleY(1, 0.5f).SetEase(Ease.OutBack));
+        animateSequence.Join(tent.transform.DOScaleX(1, 0.7f).SetEase(Ease.OutBack));
+        animateSequence.Append(title[4].transform.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack));
+        animateSequence.AppendCallback(() =>
+        {
+            title[0].transform.DOShakeRotation(3, 3, 1).SetLoops(int.MaxValue,LoopType.Yoyo);
+            title[1].transform.DOShakeRotation(3, 3, 1).SetLoops(int.MaxValue, LoopType.Yoyo);
+            title[2].transform.DOShakeRotation(3, 3, 1).SetLoops(int.MaxValue, LoopType.Yoyo); ;
+            title[3].transform.DOShakeRotation(3, 3,1).SetLoops(int.MaxValue, LoopType.Yoyo);
+            tent.transform.DOShakeScale(3, 0.1f, 1).SetLoops(int.MaxValue, LoopType.Yoyo);
+        });
+
+
         //Button
         animateSequence.Append(startButton.transform.DOScale(Vector3.one, 1f).SetEase(Ease.OutElastic));
         animateSequence.Join(optionButton.transform.DOScale(Vector3.one, 1f).SetDelay(0.1f).SetEase(Ease.OutElastic));
